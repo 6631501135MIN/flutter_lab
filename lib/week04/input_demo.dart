1db9f333366c0e286759affc1eb8fc64c8e42220
6631501135MIN <6631501135@lamduan.mfu.ac.th>
2025-09-05 11:27:51 +0700

    Update the textfield with decoration and clear button

--- lib/week04/input_demo.dart
+++ lib/week04/input_demo.dart
@@ -4,25 +4,44 @@ class InputDemo extends StatefulWidget {
   const InputDemo({super.key});
 
   @override
-  State<InputDemo> createState() => _InputDemoState();  // Fixed: Added < and _
+  State<InputDemo> createState() => _InputDemoState(); // Fixed: Added < and _
 }
 
-class _InputDemoState extends State<InputDemo> {  // Fixed: Removed extra space
-  String message = '' ;
+class _InputDemoState extends State<InputDemo> {
+  // Fixed: Removed extra space
+  //State variables
+  String message = '';
+  TextEditingController tcName = TextEditingController();
+
+  //functions
+  void updateText() {
+    setState(() {
+      message = tcName.text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Input Demo')),
       body: Column(
         children: [
-          TextField(
-            onChanged: (String txt){
-              // debugPrint(txt);
-              setState((){
-                message = txt;
-              });
-            },
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter your name',
+                suffixIcon: IconButton(
+                  onPressed: tcName.clear,
+                  icon: Icon(Icons.clear),
+                ),
+              ),
+              controller: tcName,
+            ),
           ),
+          ElevatedButton(onPressed: () => updateText(), child: Text('OK')),
+          SizedBox(height: 16),
           Text(message),
         ],
       ),

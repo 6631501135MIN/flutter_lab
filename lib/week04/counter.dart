a8d0819f1e2ce27067c3f88cbd33604a435e3e9a
6631501135MIN <6631501135@lamduan.mfu.ac.th>
2025-09-05 10:47:23 +0700

    Complete the counter app

--- lib/week04/counter.dart
+++ lib/week04/counter.dart
@@ -9,17 +9,19 @@ class Counter extends StatefulWidget {
 
 class _CounterState extends State<Counter> {  // Fixed: Changed from _MyWidgetState extends State<MyWidget>
   //state variables
-  String message = 'Hello friends';
+  // 
+  int count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Counter App', style: TextStyle(color: Colors.white)),
-          backgroundColor: Color.fromARGB(255, 1, 1, 20),
+          backgroundColor: Color.fromARGB(255, 152, 79, 192),
         ),
         body: Center(
           child: Text(
-            message,
+            // message,
+            'Counter = $count',
             style: TextStyle(fontSize: 22, color: Color.fromARGB(255, 1, 1, 20)),
           ),
         ),
@@ -27,10 +29,11 @@ class _CounterState extends State<Counter> {  // Fixed: Changed from _MyWidgetSt
           onPressed: () {
             // debugPrint('test');
             setState((){
-              message = (message == 'Hello friends') ? 'Hello me' : 'Hello friends';
+              // message = (message == 'Hello friends') ? 'Hello me' : 'Hello friends';
+              count++;
             });
           },
-          backgroundColor: Color.fromARGB(255, 1, 1, 20),
+          backgroundColor: Color.fromARGB(255, 152, 79, 192),
           child: Icon(Icons.add, color: Colors.white),
         ),
       );

1cd5f3fea4ca5debbf5197e45099d0c21fbe1f07
6631501135MIN <6631501135@lamduan.mfu.ac.th>
2025-09-12 13:01:46 +0700

    Add random_demo.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lab/week05/design2.dart';
+import 'package:flutter_lab/week05/random_demo.dart';
 
 void main() {
-  runApp(MaterialApp(home: Design2()));
+  runApp(MaterialApp(home: RandomDemo()));
 }

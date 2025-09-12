eca550b4e8e4099d85701a8a0c1a521dfd28a953
6631501135MIN <6631501135@lamduan.mfu.ac.th>
2025-09-12 11:15:26 +0700

    Add new profile design

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lab/week05/design1.dart';
+import 'package:flutter_lab/week05/design2.dart';
 
 void main() {
-  runApp(MaterialApp(home: Design1()));
+  runApp(MaterialApp(home: Design2()));
 }

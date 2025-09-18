ee00239cec1f5a8fa739c5b6c997a7b5a04e9c23
6631501135MIN <6631501135@lamduan.mfu.ac.th>
2025-09-18 22:32:56 +0700

    finished the assignments

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lab/week05/random_demo.dart';
+import 'package:flutter_lab/week05/assignment3.dart';
 
 void main() {
-  runApp(MaterialApp(home: RandomDemo()));
+  runApp(MaterialApp(home: Assignment3()));
 }
---
+++ lib/week05/assignment1.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class Assignment1 extends StatefulWidget {
+  const Assignment1({super.key});
+
+  @override
+  State<Assignment1> createState() => _Assignment1State();
+}
+
+class _Assignment1State extends State<Assignment1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink[200],
+
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(),
+            Center(
+              child: Column(
+                children: [
+                  const Text(
+                    'Wedding Organizer',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontFamily: "Sevillana",
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  SizedBox(height: 8),
+
+                  const Text(
+                    'Pre-wedding. Photo, Party',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 26,
+                      fontFamily: "Sevillana",
+                    ),
+                  ),
+
+                  SizedBox(height: 16),
+
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                    ),
+                    child: Text(
+                      'Our Services',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                '345 Moo 1 Thasud Chiang Rai, Thailand',
+                style: TextStyle(color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

1d729d3c5fbff5b9684ac777f9342240d070730d
6631501135MIN <6631501135@lamduan.mfu.ac.th>
2025-09-20 00:48:37 +0700

    Add button demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lab/week06/button_demo.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App', style: TextStyle(color: Colors.grey)),
-          backgroundColor: Color.fromARGB(255, 3, 3, 139),
-        ),
-        body: Center(
-          child: Text(
-            'Hello world',
-            style: TextStyle(fontSize: 22, color: Color.fromARGB(255, 3, 3, 139)),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          backgroundColor: Color.fromARGB(255, 3, 3, 139),
-          child: Icon(Icons.add, color: Colors.grey),
-        ),
-      ),
-    ),
-  );
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: ButtonDemo()));
 }

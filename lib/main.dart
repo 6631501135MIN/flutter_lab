ce5a9cb10143221e90e2d001b3117369a7cbdc28
6631501135MIN <6631501135@lamduan.mfu.ac.th>
2025-09-22 11:41:09 +0700

    Add image demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lab/week06/button_demo.dart';
+import 'package:flutter_lab/week06/image_demo.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: ButtonDemo()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: ImageDemo()));
 }

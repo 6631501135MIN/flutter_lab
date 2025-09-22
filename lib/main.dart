f76e6bdf98ac545511cfc187c182f4e02882ebde
6631501135MIN <6631501135@lamduan.mfu.ac.th>
2025-09-22 22:44:27 +0700

    Add baking app demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lab/week06/image_demo.dart';
+import 'package:flutter_lab/week06/baking_app.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: ImageDemo()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: BakingApp()));
 }

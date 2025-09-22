3bc152e5b1bfce9ab245baa24f4880409a8b2243
6631501135MIN <6631501135@lamduan.mfu.ac.th>
2025-09-23 00:08:46 +0700

    add login_dynamic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lab/week06/baking_app.dart';
+import 'package:flutter_lab/week06/login_dynamic.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: BakingApp()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: LoginDynamic()));
 }
